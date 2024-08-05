ee18ac4819b349e56f2ca6e33de1c15afb48bda2
Mabc365 <mustafaasif365@hotmail.com>
2024-08-05 17:18:51 -0400

    test

--- .config/setup.ps1
+++ .config/setup.ps1
@@ -8,6 +8,7 @@ winget install -e --id Git.Git
 winget install Neovim.Neovim
 winget install junegunn.fzf
 winget install gerardog.gsudo
+winget install JEsseDuffield.lazygit 
 
 #Setting Configuration
 Write-Output Copy-This And-Please-Insure-That-You-Put-The-Dollar-Sign-Immediately-Before-env: ". $ env:USERPROFILE\.config\powershell\user_profile.ps1"
