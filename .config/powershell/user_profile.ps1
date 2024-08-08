1b2dabb5e45926743bfff3b0109df3ba066af81f
Mabc365 <mustafaasif365@hotmail.com>
2024-08-07 22:33:21 -0400

    Update user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,26 +1,28 @@
-# set PowerShell to UTF-8
+# Set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
+# Import modules
 Import-Module posh-git
+Import-Module -Name Terminal-Icons
+Import-Module PSFzf
+
+# Set up initial Oh My Posh
 $omp_config = Join-Path $PSScriptRoot ".\xube.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
-Import-Module -Name Terminal-Icons
-
-# PSReadLine
+# PSReadLine configuration
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 
-# Fzf
-Import-Module PSFzf
+# Fzf configuration
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Env
+# Environment variables
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
-# Alias
+# Aliases
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
@@ -35,7 +37,52 @@ Set-Alias r pwsh
 Set-Alias e explorer
 
 # Utilities
-function which ($command) {
-      Get-Command -Name $command -ErrorAction SilentlyContinue |
+function which {
+    param (
+        [string]$command
+    )
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
         Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-        }
+}
+
+# Define global variable to track the current prompt
+if (-not $global:CurrentPrompt) {
+    $global:CurrentPrompt = ""
+}
+
+# Function to activate Oh My Posh
+function posh {
+    if ($global:CurrentPrompt -eq 'posh') {
+        Write-Host "Oh My Posh is already active."
+        return
+    }
+
+    # Deactivate Starship if it was previously active
+    if ($global:CurrentPrompt -eq 'star') {
+        starship init pwsh | Out-Null
+    }
+
+    # Activate Oh My Posh
+    oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+    $global:CurrentPrompt = 'posh'
+}
+
+# Function to activate Starship
+function star {
+    if ($global:CurrentPrompt -eq 'star') {
+        Write-Host "Starship is already active."
+        return
+    }
+
+    # Deactivate Oh My Posh if it was previously active
+    if ($global:CurrentPrompt -eq 'posh') {
+        oh-my-posh init pwsh --config $omp_config | Out-Null
+    }
+
+    # Activate Starship
+    starship init pwsh | Invoke-Expression
+    $global:CurrentPrompt = 'star'
+}
+
+# Initialize with a default prompt if desired
+# posh # Uncomment this line to set a default prompt
