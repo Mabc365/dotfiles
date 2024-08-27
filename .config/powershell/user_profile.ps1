2be46f6dfb13aa655e812fc883fdd4af9f579a3e
Mabc365 <mustafaasif365@hotmail.com>
2024-08-26 22:48:47 -0400

    listview in predictionviewstyle

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -15,6 +15,7 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Fzf configuration
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
